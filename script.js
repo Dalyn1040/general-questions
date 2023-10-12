e1405d14bff2d2647bdb3eb742e5f5a33c899c64
Dalyn1040 <lynda.adjeroun@gmail.com>
2023-10-12 11:08:25 +0200

    if else condition

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+let isOpen = false;
+
 function displayResponse() {
+          if (isOpen) {
+          document.getElementById("buttonPlus").innerHTML ="+"
+          document.getElementById("response").style.display ="none";
+          isOpen = false;
+              
+
+} else {
+          document.getElementById("buttonPlus").innerHTML ="-";
           document.getElementById("response").style.display ="block";
+          isOpen = true;
+          }
 }
 
 document.getElementById("buttonPlus").addEventListener("click", displayResponse); 
